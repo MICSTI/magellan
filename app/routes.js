224335a8b0199afee2eadaa1d4dfe2239be2d7c0
Michael Stifter <michael.stifter2@edu.fh-joanneum.at>
2016-05-09 15:55:49 +0200

    Fixed typo

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,6 @@ var path = require("path");
 module.exports = function(app) {
     // server routes ================================================
     app.get("*", function(req, res) {
-        res.sendFile(path.resolve('public/views/index.html'));
+        res.sendFile(path.resolve('build/index.html'));
     });
 }
