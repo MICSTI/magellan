719f0531b5aaacd02490f3bf9a12b0553ec19a4d
MICSTI <stifter.michael@gmx.net>
2016-09-23 15:44:16 +0200

    Add error messages to login page

--- public/js/controllers/login.ctrl.js
+++ public/js/controllers/login.ctrl.js
@@ -3,16 +3,31 @@
 angular
     .module('magellan')
     .controller('LoginCtrl', function($scope, UserSrv, FocusSrv) {
+        $scope.message = null;
+
         $scope.login = function(username, password) {
             UserSrv.login(username, password)
                 .then(function(response) {
+                    $scope.message = null;
+
                     // inform application control about login event
                     $scope.$emit('app.login', response.data);
                 })
                 .catch(function(err) {
                     console.error(err);
 
-                    // TODO: notify user about error in login process
+                    var messageText;
+
+                    if (err.status === 401) {
+                        messageText = 'Falscher Benutzername oder Passwort';
+                    } else {
+                        messageText = 'Beim Einloggen scheint etwas schief gegangen zu sein';
+                    }
+
+                    $scope.message = {
+                        type: 'error',
+                        text: messageText
+                    };
                 });
         };
 
