4ed43efbc74a4e2c29e7c6c7cef49bbde52548d1
MICSTI <stifter.michael@gmx.net>
2016-10-08 14:19:04 +0200

    Change mongoose Promise library to JavaScript global Promise library

--- db.js
+++ db.js
@@ -3,12 +3,16 @@ var mongoose = require('mongoose');
 // config files ====================================
 var DB = require("./config/db");
 
+// to solve deprecation problem of mongoose's mpromise library
+// see: https://github.com/Automattic/mongoose/issues/4291
+mongoose.Promise = global.Promise;
+
 mongoose.connect(DB.url, function(err) {
     if (err) {
         throw Error("Could not connect to MongoDB");
     }
 
-    console.log('mongodb connected');
+    console.log('MongoDB connected');
 });
 
 module.exports = mongoose;
