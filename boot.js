4c61cecd3e01e77a2a66954e4f50129b69066527
MICSTI <stifter.michael@gmx.net>
2016-10-10 08:53:03 +0200

    Add log output of node environment variable

--- boot.js
+++ boot.js
@@ -1,6 +1,8 @@
 var cluster = require("cluster");
 var numCpus = require("os").cpus().length;
 
+console.log('Environment:', process.env.NODE_ENV || 'development');
+
 cluster.setupMaster({ exec: __dirname + "/server.js" });
 
 for (var i = 0; i < numCpus; i++) {
