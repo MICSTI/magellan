58139606b57f9cec36bf92e621eaaa30d9221d7b
MICSTI <stifter.michael@gmx.net>
2016-10-10 16:55:31 +0200

    Remove log statement from successful event post case

--- public/js/services/event.service.js
+++ public/js/services/event.service.js
@@ -7,7 +7,7 @@ angular
             $http.post('/api/event/add', {
                 name: name
             }).success(function(data) {
-                LogSrv.info('successfully added event', name);
+                // no need to react to success case
             }).catch(function(err) {
                 LogSrv.error('failed to add event', err);
             });
