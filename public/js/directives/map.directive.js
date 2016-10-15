70451f657830c02f2dc303b22d2616102dee7fe1
MICSTI <stifter.michael@gmx.net>
2016-10-15 17:30:51 +0200

    Add configuration to world map

--- public/js/directives/map.directive.js
+++ public/js/directives/map.directive.js
@@ -10,9 +10,14 @@ angular
             link: function link(scope, element, attrs) {
                 var map = new Datamap({
                     element: document.getElementById('map-container'),
+                    fills: {
+                        defaultFill: '#22a7f0'
+                    },
                     geographyConfig: {
                         highlightOnHover: true,
-                        popuponHover: true
+                        popuponHover: true,
+                        highlightFillColor: '#019875',
+                        highlightBorderColor: 'rgba(200, 247, 197, 0.4)'
                     }
                 });
             }
