76739c72bcad48135710cd9403f90d975ba5cfa0
Michael Stifter <stifter.michael@gmx.net>
2017-08-14 19:52:06 +0200

    Finish frontend functionality of Facebook OAuth login

--- controllers/api/server-auth.js
+++ controllers/api/server-auth.js
@@ -366,7 +366,7 @@ router.get('/user', protectRoute, function(req, res) {
 /**
  * Facebook login (OAuth)
  */
-router.get('/auth/facebook', passport.authorize('facebook', { scope: ['email'] }));
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 router.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
             successRedirect: '/home',
--- public/js/controllers/login.ctrl.js
+++ public/js/controllers/login.ctrl.js
@@ -2,7 +2,7 @@
 
 angular
     .module('magellan')
-    .controller('LoginCtrl', function($scope, UserSrv, FocusSrv) {
+    .controller('LoginCtrl', function($scope, $window, UserSrv, FocusSrv) {
         $scope.message = null;
 
         $scope.login = function(username, password) {
@@ -29,6 +29,10 @@ angular
                         text: messageText
                     };
                 });
+        };
+
+        $scope.doFacebookLogin = function() {
+            $window.location.assign('/api/auth/facebook');
         };
 
         // focus username field
