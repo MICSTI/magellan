93c8f4d88b9235f32f695f06cf54f70ad40ec678
Michael Stifter <stifter.michael@gmx.net>
2017-09-19 20:47:26 +0200

    Add ARIA attributes to selectable component

--- public/js/directives/selectable.directive.js
+++ public/js/directives/selectable.directive.js
@@ -12,13 +12,28 @@ angular
                 isSelected: '=isSelected'
             },
             link: function(scope, element, attrs) {
+                var setRole = function() {
+                    attrs.$set('role', 'checkbox');
+                };
+
+                var setAriaChecked = function() {
+                    attrs.$set('ariaChecked', scope.isSelected);
+                };
+
                 scope.toggleState = function() {
                     // toggle the "isSelected" flag
                     scope.isSelected = !scope.isSelected;
 
                     // set the attribute on the HTML element
                     attrs.$set('isSelected', scope.isSelected);
+
+                    // also set the aria checked attribute
+                    setAriaChecked();
                 };
+
+                // initially set the aria attributes
+                setRole();
+                setAriaChecked();
             }
         }
     });
