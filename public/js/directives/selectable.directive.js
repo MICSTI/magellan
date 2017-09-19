f521b04d2dc4a656dd825048c15c8e98135312c5
Michael Stifter <stifter.michael@gmx.net>
2017-09-19 17:28:40 +0200

    Finish basic implementation of selectable component

--- public/js/directives/selectable.directive.js
+++ public/js/directives/selectable.directive.js
@@ -2,14 +2,23 @@
 
 angular
     .module('magellan')
-    .directive('selectable', function() {
+    .directive('selectable', function($compile) {
         return {
             restrict: 'E',
             templateUrl: 'dist/views/templates/selectable.template.html',
-            controller: 'SelectableCtrl',
             scope: {
                 countryName: '@countryName',
-                countryAlpha2: '@countryAlpha2'
+                countryAlpha2: '@countryAlpha2',
+                isSelected: '=isSelected'
+            },
+            link: function(scope, element, attrs) {
+                scope.toggleState = function() {
+                    // toggle the "isSelected" flag
+                    scope.isSelected = !scope.isSelected;
+
+                    // set the attribute on the HTML element
+                    attrs.$set('isSelected', scope.isSelected);
+                };
             }
         }
     });
